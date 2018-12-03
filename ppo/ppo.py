6dfe0f25f7ee809117b1505795baf940858c5a00
Daniel Palenicek <daniel.palenicek@gmail.com>
2018-12-03 19:15:24 +0100

    first version of PPO

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -6,61 +6,43 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical, Normal
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 import gym
 import quanser_robots
 
-
-parser = argparse.ArgumentParser(description='Solve the different gym envs with PPO')
-parser.add_argument('--render', action='store_true', help='render the environment')
-args = parser.parse_args()
-
-torch.set_printoptions(threshold=5000)
-
-
 class PolicyNetwork(nn.Module):
-    """
-    Proximal Policy Optimization algorithm.
-    Paper: https://arxiv.org/abs/1707.06347
-    """
 
     def __init__(self, env):
         super(PolicyNetwork, self).__init__()
-
         self.observation_space = env.observation_space
         self.action_space = env.action_space
+        self.action_scaling = torch.tensor(self.action_space.high, dtype=torch.float32)
 
         self.h1 = nn.Linear(self.observation_space.shape[0], 100)
-        #self.h2 = nn.Linear(100, 100)
-
         self.action_mu  = nn.Linear(100, 1)
         self.action_sig = nn.Linear(100, 1)
-        #self.value = nn.Linear(100, 1)
 
     def forward(self, state):
-        # print('state', state.shape)
-        x = torch.from_numpy(state).float().unsqueeze(0)
-        # print('x', x)
-        x = torch.tanh(self.h1(x))
-        # x = F.relu(self.h2(x))
+        x = F.relu(self.h1(state))
+        μ = self.action_scaling * torch.tanh(self.action_mu(x))
+        σ = F.softplus(self.action_sig(x)) + 1e-5 # make sure we never predict zero variance.
+        return μ, σ
 
-        # tanh gives values between [-1,1]. So we scale μ according to the
-        # environment's specifications.
-        # print('tensor', torch.tensor(self.action_space.high, dtype=torch.float32))
-        μ = torch.tensor(self.action_space.high, dtype=torch.float32) * torch.tanh(self.action_mu(x))
-        σ = F.softplus(self.action_sig(x)) + 0.01 # make sure we never predict zero variance.
-        #value = self.value(x)
-        # print('μ', μ)
-        # print('σ', σ)
-        # print()
-        return μ.view(-1), σ.view(-1) #, value
+    def select_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        with torch.no_grad():
+            μ, σ = self.forward(state)
+        a_dist = Normal(μ, σ)
+        action = a_dist.sample()
+        log_prob = a_dist.log_prob(action)
+        return action.item(), log_prob.item()
 
 
 class ValueNetwork(nn.Module):
 
     def __init__(self, env):
         super(ValueNetwork, self).__init__()
-
         self.observation_space = env.observation_space
         self.action_space = env.action_space
 
@@ -68,165 +50,191 @@ class ValueNetwork(nn.Module):
         self.value = nn.Linear(100, 1)
 
     def forward(self, state):
-        x = torch.from_numpy(state).float().unsqueeze(0)
-        x = torch.tanh(self.h1(x))
-        value = self.value(x).view(-1)
-        # print('value', value)
+        #x = torch.from_numpy(state).float().unsqueeze(0)
+        x = F.relu(self.h1(state))
+        value = self.value(x)
         return value
 
-
-def train():
-
-    env = gym.make('Pendulum-v0')
-    # env = gym.make('DoublePendulum-v0') # DoubleCartPolea
-    # env = gym.make('Qube-v0')  # FurutaPend
-    # env = gym.make('BallBalancerSim-v0')  # BallBalancer
-
-    model   = PolicyNetwork(env).float()
-    model_v = ValueNetwork(env).float()
-
-    optimizer   = optim.Adam(model.parameters(), lr=1e-3)
-    optimizer_v = optim.Adam(model_v.parameters(), lr=1e-3)
-
-    print('Env:', env)
-    print('Reward range', env.reward_range)
-    print('Observation space:', env.observation_space)
-    print('  ', env.observation_space.high)
-    print('  ', env.observation_space.low)
-    print('Action space:', env.action_space)
-    print('  ', env.action_space.high)
-    print('  ', env.action_space.low)
-
-    for epoch in range(100000):
-        state = env.reset()
-        states, next_states, actions, log_probs, rewards, values, dones = [],[],[],[],[],[],[]
-
-        traj_length = 0
-        traj_lengths = []
-
-        # Sample trajectories
-        n_steps = 2000
-        for step in range(n_steps):
-
-            #print(f"  step {step+1:4}/{n_steps}", end="\r")
-
-            # Sample from network
-            (μ, σ), value = model(state), model_v(state)
-            # print('(μ, σ), value', (μ, σ), value)
-            # print()
-
-            a_dist = Normal(μ, σ)
-            action = a_dist.sample()
-            log_prob = a_dist.log_prob(action)
-            a = action.detach().numpy()
-            # print('a', a)
-            state_, reward, done, _ = env.step(a)
-
-            # print('state_', state_)
-
-            # since rendering is expensive only render sometimes
-            if args.render and epoch%20==0 and epoch > 0 and step < 500:
-                env.render()
-
-            states.append(state)
-            next_states.append(state_)
-            actions.append(action)
-            log_probs.append(log_prob)
-            rewards.append(reward)
-            values.append(value)
-            dones.append(done)
-
-            traj_length += 1
-            state = state_
-
-            if done:
-                state = env.reset()
-                traj_lengths.append(traj_length)
-                traj_length = 0
-
-        traj_lengths.append(traj_length)
-
-        # Update Policy
-        γ = 0.95
-        returns = []
-        # R = model(state)[2].detach().item()
-        R = model_v(state).item()
-        for t in reversed(range(n_steps)):
-            R = rewards[t] + (1-dones[t]) * γ * R
-            returns.insert(0,R)
-
-        dones = torch.tensor(dones, dtype=torch.float32).view(-1,1)
-        values_ = torch.tensor(values, dtype=torch.float32).view(-1,1)
-        rewards = torch.tensor(rewards, dtype=torch.float32).view(-1,1)
-        returns = torch.tensor(returns, dtype=torch.float32).view(-1,1)
-        advantages = returns - values_
-
-        policy_losses = []
-        value_losses = []
-        for log_prob, adv, value, r in zip(log_probs, advantages, values, returns):
-            policy_losses.append(-log_prob * adv.item())
-            value_losses.append( (value - r)**2 )
-
-        policy_loss = torch.stack(policy_losses).sum()
-        value_loss = torch.stack(value_losses).sum()
-        loss = policy_loss + value_loss
-
-        #############################
-        # Single network optimization
-        #############################
-
-        # optimizer.zero_grad()
-        #
-        # # for f in model.parameters():
-        # #     print()
-        # #     print(f'{f.data.size()} grad is')
-        # #     print(f.grad)
-        #
-        # loss.backward()
-        #
-        # # for f in model.parameters():
-        # #     print()
-        # #     print(f'{f.data.size()} grad is')
-        # #     print(f.grad)
-        #
-        # optimizer.step()
+    def get_value(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        value = self.forward(state)
+        return value.item()
 
 
-        ###########################
-        # Two networks optimization
-        ###########################
+parser = argparse.ArgumentParser(description='Solve the different gym envs with PPO')
+parser.add_argument('--eval', action='store_true', help='toggles evaluation mode')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--resume', action='store_true', help='resume training on an existing model by loading the last checkpoint')
+args = parser.parse_args()
 
-        optimizer.zero_grad()
-        # for f in model.parameters():
-        #     print()
-        #     print(f'{f.data.size()} grad is')
-        #     print(f.grad)
-        policy_loss.backward()
-        # for f in model.parameters():
-        #     print()
-        #     print(f'{f.data.size()} grad is')
-        #     print(f.grad)
-        optimizer.step()
+torch.set_printoptions(threshold=5000)
 
-        optimizer_v.zero_grad()
-        # print('Before Backward')
-        # for f in model_v.parameters():
-        #     print()
-        #     print(f'{f.data.size()} grad is')
-        #     print(f.grad)
-        value_loss.backward()
-        # print('\n\nAfter Backward')
-        # for f in model_v.parameters():
-        #     print()
-        #     print(f'{f.data.size()} grad is')
-        #     print(f.grad)
-        optimizer_v.step()
+training = not args.eval
 
 
 
+env = gym.make('Pendulum-v0')
+# env = gym.make('DoublePendulum-v0') # DoubleCartPole
+# env = gym.make('Qube-v0')  # FurutaPend
+# env = gym.make('BallBalancerSim-v0')  # BallBalancer
+print('Env:', env)
+print('Reward range', env.reward_range)
+print('Observation space:', env.observation_space)
+print('  ', env.observation_space.high)
+print('  ', env.observation_space.low)
+print('Action space:', env.action_space)
+print('  ', env.action_space.high)
+print('  ', env.action_space.low)
 
-        print(f"epoch {epoch:3} -> steps {np.mean(traj_lengths):4.0f}  | rewards {torch.sum(rewards)/len(traj_lengths):8.4f} {torch.sum(rewards):8.0f} |   loss {loss:12.2f}  |   policy {policy_loss:12.2f}  |   value {value_loss:8.2f}")
+# torch.manual_seed(0)
+# env.seed(0)
+
+model   = PolicyNetwork(env).float()
+model_v = ValueNetwork(env).float()
+optimizer   = optim.Adam(model.parameters(), lr=1e-4)
+optimizer_v = optim.Adam(model_v.parameters(), lr=3e-4)
+epoch = -1
+
+if args.eval or args.resume:
+    checkpoint = torch.load('out/ppo_checkpoint.pt')
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model_v.load_state_dict(checkpoint['model_v_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    optimizer_v.load_state_dict(checkpoint['optimizer_v_state_dict'])
+    epoch = checkpoint['epoch']
+    print(f"-> LOADED MODEL at epoch {epoch}")
 
 
-if __name__ == '__main__':
-    train()
+if training:
+    model.train()
+    model_v.train()
+else:
+    model.eval()
+    model_v.eval()
+
+
+while epoch < 100000:
+    epoch += 1
+    state = env.reset()
+    states, next_states, actions, log_probs, rewards, values, dones = [],[],[],[],[],[],[]
+
+    traj_length = 0
+    traj_lengths = []
+
+    # Sample trajectories
+    n_steps = 1000
+    for step in range(n_steps):
+
+        action, log_prob = model.select_action(state)
+        state_, reward, done, _ = env.step(np.array([action]))
+
+        # since rendering is expensive only render sometimes
+        if args.render:
+            env.render()
+
+        states.append(state)
+        next_states.append(state_)
+        actions.append(action)
+        log_probs.append(log_prob)
+        rewards.append(reward)
+        # values.append(value)
+
+        dones.append(done)
+
+        traj_length += 1
+        state = state_
+
+        if done:
+            state = env.reset()
+            traj_lengths.append(traj_length)
+            traj_length = 0
+
+    traj_lengths.append(traj_length)
+
+    # print()
+    # print('states', states)
+    # print('next_states', next_states)
+    # print('actions', actions)
+    # print('log_probs', log_probs)
+    # print('rewards', rewards)
+    # print('values', values)
+    # print('dones', dones)
+    # print()
+
+    # calculate returns
+    #################
+    # Update Policy #
+    #################
+
+    γ = 0.9
+    num_ppo_epochs = 10
+    batch_size = 128
+
+    states = torch.tensor(states, dtype=torch.float32)
+    actions = torch.tensor(actions, dtype=torch.float32).view(-1,1)
+    next_states = torch.tensor(next_states, dtype=torch.float32)
+    old_log_probs = torch.tensor(log_probs, dtype=torch.float32).view(-1,1)
+    rewards = torch.tensor(rewards, dtype=torch.float32).view(-1,1)
+    normalized_rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+
+    # REVIEW: use 1-step GAE vs. discounted rewards for return calculation.
+
+    # R = model_v.get_value(state)
+    # returns = []
+    # for t in reversed(range(n_steps)):
+    #     R = normalized_rewards[t] + (1-dones[t]) * γ * R
+    #     returns.insert(0,R)
+    # returns = torch.tensor(returns, dtype=torch.float32).view(-1,1)
+
+    with torch.no_grad():
+        returns = normalized_rewards + γ * model_v(next_states)
+        # returns = rewards + γ * model_v(next_states)
+        # returns = (returns - returns.mean()) / (returns.std() + 1e-5)
+        advs = returns - model_v(states)
+
+    policy_losses, value_losses = [], []
+
+    if training:
+        for _ in range(num_ppo_epochs):
+            for index in BatchSampler(SubsetRandomSampler(range(n_steps)), batch_size, False):
+
+                # Construct the mini batch
+                mb_states = states[index]
+                mb_actions = actions[index]
+                mb_old_log_probs = old_log_probs[index]
+                mb_returns = returns[index]
+                mb_advs = advs[index]
+
+                μ, σ = model(mb_states)
+                a_dist = Normal(μ, σ)
+                mb_log_probs = a_dist.log_prob(mb_actions)
+                ratio = torch.exp(mb_log_probs - mb_old_log_probs)
+
+                optimizer.zero_grad()
+                surr1 = ratio * mb_advs
+                surr2 = torch.clamp(ratio, 0.8, 1.2) * mb_advs
+                policy_loss = -torch.min(surr1, surr2).mean()
+                policy_loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                optimizer.step()
+
+                optimizer_v.zero_grad()
+                value_loss = F.smooth_l1_loss(model_v(mb_states), mb_returns)
+                value_loss.backward()
+                nn.utils.clip_grad_norm_(model_v.parameters(), 0.5)
+                optimizer_v.step()
+
+                policy_losses.append(policy_loss.item())
+                value_losses.append(value_loss.item())
+
+        model_states = {
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'model_v_state_dict': model_v.state_dict(),
+            'optimizer_v_state_dict': optimizer_v.state_dict(),
+        }
+        torch.save(model_states, './out/ppo_checkpoint.pt')
+
+    if epoch%10==0:
+        print(f"{epoch:4} rewards {rewards.sum().item():10.2f} | policy {np.mean(policy_losses):12.3f} | value {np.mean(value_losses):12.3f}")
