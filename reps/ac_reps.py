8e32bb08566202ae673d70fda236586638e651e5
Daniel Palenicek <daniel.palenicek@gmail.com>
2019-02-20 13:32:53 +0100

    optimized ACREPS and using multivariate policy

--- reps/ac_reps.py
+++ reps/ac_reps.py
@@ -1,7 +1,9 @@
 import os
 
 import torch
-import numpy as np
+# import numpy as np
+import autograd.numpy as np
+from autograd import grad, jacobian
 from scipy.optimize import minimize
 
 from tensorboardX import SummaryWriter
@@ -40,7 +42,8 @@ class ACREPS:
             file = np.load(self.checkpoint_path)
             self.fourier_features = file['fourier_features']
             self.θ = file['θ']
-            self.σ = file['σ'] if self.resume else 0.0  # zero variance for evaluation
+            # self.σ = file['σ'] if self.resume else 0.0  # zero variance for evaluation
+            self.Σ = file['Σ'] if self.resume else np.zeros_like(file['Σ'])  # zero variance for evaluation
             self.α = file['α']
             self.η = file['η']
             self.epoch = file['epoch']
@@ -58,7 +61,7 @@ class ACREPS:
                 fourier_feature_parameters.append((freq, offset))
             self.fourier_features = np.array(fourier_feature_parameters)
             self.θ = np.random.randn(n_fourier, self.env.action_space.shape[0])
-            self.σ = 100.0 #16.0
+            self.Σ = np.array([[16.0**2]])  # TODO: Adapt dimemsion to environment
             self.α = np.random.randn(n_fourier)
             self.η = np.random.rand()
             self.epoch = 0
@@ -70,8 +73,9 @@ class ACREPS:
         :param state: environment state
         :return: feature vector for state
         """
-        feature_vector = [np.sin(f @ (state + o)) for f, o in self.fourier_features]
-        return np.array(feature_vector)
+        freq = self.fourier_features[:, 0, :]
+        offset = self.fourier_features[:, 1, :]
+        return np.sin(np.sum(freq * (state + offset), axis=-1))
 
     def train(self):
         while self.epoch < self.n_epochs or self.eval:
@@ -80,30 +84,27 @@ class ACREPS:
             ############################
 
             self.epoch += 1
-            states, next_states, actions, rewards, dones = [], [], [], [], []
+            states, actions, rewards, dones = [], [], [], []
 
-            state = self.env.reset()
+            φ_s = self.φ_fn(self.env.reset())
 
             # Sample trajectories
             for step in range(self.n_steps):
 
-                # action = np.random.multivariate_normal(mean=φ_fn(state).T @ θ, cov=σ)
-                action = np.random.normal(loc=self.φ_fn(state).T @ self.θ, scale=self.σ)
-                state_, reward, done, _ = self.env.step(action)
+                action = np.random.multivariate_normal(mean=φ_s.T@self.θ, cov=self.Σ)
+                next_state, reward, done, _ = self.env.step(action)
+                states.append(φ_s)
+                actions.append(action)
+                rewards.append(reward)
+                dones.append(done)
 
                 if self.render:
                     self.env.render()
 
-                states.append(self.φ_fn(state))
-                actions.append(action)
-                rewards.append(reward)
-                next_states.append(self.φ_fn(state_))
-                dones.append(done)
-
                 if done:
-                    state = self.env.reset()
+                    φ_s = self.φ_fn(self.env.reset())
                 else:
-                    state = state_
+                    φ_s = self.φ_fn(next_state)
                 print(f"step {step}", end="\r")
 
             if self.training:
@@ -123,13 +124,14 @@ class ACREPS:
                 def dual(p):
                     """dual formulation of the ACREPS objective function"""
                     η, α = p[0], p[1:]
-                    δ = returns - α.dot(φ.T)
-                    return η * self.ε + np.max(δ) + η * np.log(np.mean(np.exp((δ - np.max(δ)) / η))) + np.mean(
-                        α.dot(φ.T)) + 1e-9 * np.linalg.norm(α, 2)
+                    V_s = np.dot(α, φ.T)
+                    δ = returns - V_s
+                    max_δ = np.max(δ)
+                    return η * self.ε + max_δ + η * np.log(np.mean(np.exp((δ - max_δ) / η))) + np.mean(V_s) + 1e-9 * np.linalg.norm(α, 2)
 
                 params = np.concatenate([np.array([self.η]), self.α])
                 bounds = [(1e-8, None)] + [(None, None)] * len(self.α)  # bounds for η and α
-                res = minimize(dual, params, method='SLSQP', bounds=bounds)
+                res = minimize(dual, params, method='SLSQP', jac=jacobian(dual), bounds=bounds)
                 self.η, self.α = res.x[0], res.x[1:]
 
                 ############################
@@ -146,18 +148,24 @@ class ACREPS:
                 Φ = np.array(states)
                 a = np.array(actions)
 
-                # Update policy parameters
-                self.θ = np.linalg.solve(Φ.T @ W @ Φ + 1e-9 * np.eye(Φ.shape[-1]), Φ.T @ W @ a)
-                Z = (np.square(np.sum(ω)) - np.sum(np.square(ω))) / np.sum(ω)
-                self.σ = np.sqrt(np.sum(W @ np.square(a - Φ @ self.θ)) / Z)
-                # self.σ = 0.5 * self.σ + 0.5 * np.sqrt(np.sum(W @ np.square(a - Φ @ self.θ)) / Z)
-                # σ = np.eye(env.action_space.shape[0]) * np.sum(W @ np.square(ã - Φ @ θ), axis=0) / Z
-
                 self.writer.add_scalar('rl/reward', torch.tensor(np.sum(rewards), dtype=torch.float32), self.epoch)
+                self.writer.add_scalar('rl/entropy', torch.tensor(normal_entropy(self.Σ), dtype=torch.float32), self.epoch)
                 self.writer.add_scalar('rl/η', torch.tensor(self.η), self.epoch)
                 self.writer.add_scalar('rl/KL', torch.tensor(self.kl), self.epoch)
 
-                np.savez(self.checkpoint_path, θ=self.θ, α=self.α, η=self.η, σ=self.σ,
+                # Update policy parameters
+                self.θ = np.linalg.solve(Φ.T @ W @ Φ + 1e-9 * np.eye(Φ.shape[-1]), Φ.T @ W @ a)
+                z = (np.square(np.sum(ω)) - np.sum(np.square(ω))) / np.sum(ω)
+                # self.σ = np.sqrt(np.sum(W @ np.square(a - Φ @ self.θ)) / Z)
+                self.Σ = np.eye(self.env.action_space.shape[0]) * np.sum(W @ np.square(a - Φ @ self.θ), axis=0) / z
+
+                np.savez(self.checkpoint_path, θ=self.θ, α=self.α, η=self.η, Σ=self.Σ,
                          fourier_features=self.fourier_features, epoch=self.epoch)
 
-            print(f"{self.epoch:4} rewards {np.sum(rewards):13.6f} | KL {self.kl:8.6f} | σ {self.σ}")
+            print(f"{self.epoch:4} rewards {np.sum(rewards):13.6f} | KL {self.kl:8.6f} | Σ {self.Σ} | entropy {normal_entropy(self.Σ)}")
+
+
+def normal_entropy(Σ):
+    """Calculates the entropy of a gaussian policy given it's covariance matrix Σ."""
+    return 0.5 * np.log(2*np.e*np.pi*np.linalg.det(Σ))
+
